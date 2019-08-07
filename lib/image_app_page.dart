71b5c4f1c27e2b2fac7d1f6c05dd8978e89df9b9
renxiaoya <renxiaoya@shuidihuzhu.com>
2019-08-07 15:43:48 +0800

    change to mirical for clock up

--- lib/image_app_page.dart
+++ lib/image_app_page.dart
@@ -1,114 +1,114 @@
-import 'dart:io';
-
-import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
-
-///拍照app页面
-class ImageAppPage extends StatefulWidget {
-  ImageAppPage({Key key}) : super(key: key);
-
-  _ImageAppPageState createState() => _ImageAppPageState();
-}
-
-class _ImageAppPageState extends State<ImageAppPage> {
-  List<File> _images = [];
-
-  Future getImage(bool isTakePhoto) async {
-    Navigator.pop(context);
-    var image = await ImagePicker.pickImage(
-        source: isTakePhoto ? ImageSource.camera : ImageSource.gallery);
-    if (image != null) {
-      setState(() {
-        _images.add(image);
-      });
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('拍照app'),
-      ),
-      body: Center(
-        child: Wrap(
-          spacing: 5,
-          runSpacing: 5,
-          children: _genImage(),
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _pickImage,
-        tooltip: '选择图片',
-        child: Icon(Icons.add_a_photo),
-      ),
-    );
-  }
-
-  _pickImage() {
-    showModalBottomSheet(
-        context: context,
-        builder: (context) => Container(
-              height: 160,
-              child: Column(
-                children: <Widget>[
-                  _item('拍照', true),
-                  _item('从相册中选择', false),
-                ],
-              ),
-            ));
-  }
-
-  _item(String title, bool isTakePhoto) {
-    return GestureDetector(
-      child: ListTile(
-        leading: Icon(isTakePhoto ? Icons.camera_alt : Icons.photo_library),
-        title: Text(title),
-        onTap: () => getImage(isTakePhoto),
-      ),
-    );
-  }
-
-  _genImage() {
-    return _images.map((file) {
-      return Stack(
-        children: <Widget>[
-          ClipRRect(
-            //圆角
-            borderRadius: BorderRadius.circular(5),
-            child: Image.file(
-              file,
-              width: 120,
-              height: 90,
-              fit: BoxFit.fill,
-            ),
-          ),
-          Positioned(
-            right: 5,
-            top: 5,
-            child: GestureDetector(
-              onTap: () {
-                setState(() {
-                  _images.remove(file);
-                });
-              },
-              child: ClipOval(
-                child: Container(
-                  padding: EdgeInsets.all(3),
-                  decoration: BoxDecoration(
-                    color: Colors.black54,
-                  ),
-                  child: Icon(
-                    Icons.close,
-                    size: 18,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-            ),
-          )
-        ],
-      );
-    }).toList();
-  }
-}
+//import 'dart:io';
+//
+//import 'package:flutter/material.dart';
+//import 'package:image_picker/image_picker.dart';
+//
+/////拍照app页面
+//class ImageAppPage extends StatefulWidget {
+//  ImageAppPage({Key key}) : super(key: key);
+//
+//  _ImageAppPageState createState() => _ImageAppPageState();
+//}
+//
+//class _ImageAppPageState extends State<ImageAppPage> {
+//  List<File> _images = [];
+//
+//  Future getImage(bool isTakePhoto) async {
+//    Navigator.pop(context);
+//    var image = await ImagePicker.pickImage(
+//        source: isTakePhoto ? ImageSource.camera : ImageSource.gallery);
+//    if (image != null) {
+//      setState(() {
+//        _images.add(image);
+//      });
+//    }
+//  }
+//
+//  @override
+//  Widget build(BuildContext context) {
+//    return Scaffold(
+//      appBar: AppBar(
+//        title: Text('拍照app'),
+//      ),
+//      body: Center(
+//        child: Wrap(
+//          spacing: 5,
+//          runSpacing: 5,
+//          children: _genImage(),
+//        ),
+//      ),
+//      floatingActionButton: FloatingActionButton(
+//        onPressed: _pickImage,
+//        tooltip: '选择图片',
+//        child: Icon(Icons.add_a_photo),
+//      ),
+//    );
+//  }
+//
+//  _pickImage() {
+//    showModalBottomSheet(
+//        context: context,
+//        builder: (context) => Container(
+//              height: 160,
+//              child: Column(
+//                children: <Widget>[
+//                  _item('拍照', true),
+//                  _item('从相册中选择', false),
+//                ],
+//              ),
+//            ));
+//  }
+//
+//  _item(String title, bool isTakePhoto) {
+//    return GestureDetector(
+//      child: ListTile(
+//        leading: Icon(isTakePhoto ? Icons.camera_alt : Icons.photo_library),
+//        title: Text(title),
+//        onTap: () => getImage(isTakePhoto),
+//      ),
+//    );
+//  }
+//
+//  _genImage() {
+//    return _images.map((file) {
+//      return Stack(
+//        children: <Widget>[
+//          ClipRRect(
+//            //圆角
+//            borderRadius: BorderRadius.circular(5),
+//            child: Image.file(
+//              file,
+//              width: 120,
+//              height: 90,
+//              fit: BoxFit.fill,
+//            ),
+//          ),
+//          Positioned(
+//            right: 5,
+//            top: 5,
+//            child: GestureDetector(
+//              onTap: () {
+//                setState(() {
+//                  _images.remove(file);
+//                });
+//              },
+//              child: ClipOval(
+//                child: Container(
+//                  padding: EdgeInsets.all(3),
+//                  decoration: BoxDecoration(
+//                    color: Colors.black54,
+//                  ),
+//                  child: Icon(
+//                    Icons.close,
+//                    size: 18,
+//                    color: Colors.white,
+//                  ),
+//                ),
+//              ),
+//            ),
+//          )
+//        ],
+//      );
+//    }).toList();
+//  }
+//}
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:myapp/full_use.dart';
+import 'package:myapp/http_test_wjh.dart';
 import 'package:myapp/layout_page.dart';
 import 'package:myapp/logo_app.dart';
 import 'package:myapp/photo_hero_page.dart';
 
+import 'http_test.dart';
 import 'image_app_page.dart';
 import 'less_use.dart';
 
-void main() => runApp(MaterialApp(home: HeroAnimation(),));
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
@@ -18,64 +20,65 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('flutter使用路由'),
-        ),
-        body: RouteNavigator(),
-      ),
-      routes: <String, WidgetBuilder>{
-        'less': (BuildContext context) => LessUse(),
-        'full': (BuildContext context) => FullUse(),
-        'layout': (BuildContext context) => LayoutPage(),
-        'image': (BuildContext context) => ImageAppPage()
-      },
+      home: HttpTestWjh(),
+//      home: Scaffold(
+//        appBar: AppBar(
+//          title: Text('flutter使用路由'),
+//        ),
+//        body: RouteNavigator(),
+//      ),
+//      routes: <String, WidgetBuilder>{
+//        'less': (BuildContext context) => LessUse(),
+//        'full': (BuildContext context) => FullUse(),
+//        'layout': (BuildContext context) => LayoutPage(),
+//        'image': (BuildContext context) => ImageAppPage()
+//      },
     );
   }
 }
 
-class RouteNavigator extends StatefulWidget {
-  @override
-  _RouteNavigatorState createState() => _RouteNavigatorState();
-}
-
-class _RouteNavigatorState extends State<RouteNavigator> {
-  bool byName = false;
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        children: <Widget>[
-          SwitchListTile(
-            value: byName,
-            title: Text('${byName ? '' : '不'}使用路由名跳转'),
-            onChanged: (value) {
-              setState(() {
-                byName = value;
-              });
-            },
-          ),
-          _item('Stateless基础组件', LessUse(), 'less'),
-          _item('Statefull基础组件', FullUse(), 'full'),
-          _item('Flutter layout', LayoutPage(), 'layout'),
-          _item('拍照app', ImageAppPage(), 'image'),
-        ],
-      ),
-    );
-  }
-
-  _item(String title, page, String routeName) {
-    return Container(
-      child: RaisedButton(
-        onPressed: () {
-          if (byName) {
-            Navigator.pushNamed(context, routeName);
-          }else{
-            Navigator.push(context, MaterialPageRoute(builder: (context)=>page));
-          }
-        },
-        child: Text(title),
-      ),
-    );
-  }
-}
+//class RouteNavigator extends StatefulWidget {
+//  @override
+//  _RouteNavigatorState createState() => _RouteNavigatorState();
+//}
+//
+//class _RouteNavigatorState extends State<RouteNavigator> {
+//  bool byName = false;
+//  @override
+//  Widget build(BuildContext context) {
+//    return Container(
+//      child: Column(
+//        children: <Widget>[
+//          SwitchListTile(
+//            value: byName,
+//            title: Text('${byName ? '' : '不'}使用路由名跳转'),
+//            onChanged: (value) {
+//              setState(() {
+//                byName = value;
+//              });
+//            },
+//          ),
+//          _item('Stateless基础组件', LessUse(), 'less'),
+//          _item('Statefull基础组件', FullUse(), 'full'),
+//          _item('Flutter layout', LayoutPage(), 'layout'),
+//          _item('拍照app', ImageAppPage(), 'image'),
+//        ],
+//      ),
+//    );
+//  }
+//
+//  _item(String title, page, String routeName) {
+//    return Container(
+//      child: RaisedButton(
+//        onPressed: () {
+//          if (byName) {
+//            Navigator.pushNamed(context, routeName);
+//          }else{
+//            Navigator.push(context, MaterialPageRoute(builder: (context)=>page));
+//          }
+//        },
+//        child: Text(title),
+//      ),
+//    );
+//  }
+//}
